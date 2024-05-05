88634ebcdbb23d1e5ae21161888c8cfff122355b
Gustavo Martinez de Murga <gustavo.murga1996@gmail.com>
2024-05-05 09:46:05 -0400

    adding not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { 
     path: 'books',
     loadChildren: () => import('./books/books.module').then(m => m.BooksModule) 
   },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
   { path: '', redirectTo: 'books', pathMatch: 'full' },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundComponent
+  ],
   imports: [
     CommonModule,
     MaterialModule,
