0fe5e22bb60a1f639bf87e818683753c6b09773f
Gustavo Martinez de Murga <gustavo.murga1996@gmail.com>
2024-05-04 22:08:45 -0400

    converting a standalone app to module based app.

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    SharedModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
